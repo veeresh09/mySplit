eb3e80c5da8af0e1211cc963d89144b941c6c0a3
veeresh09 <veeresh1729@gmail.com>
2024-01-27 17:16:00 -0600

    connected homescreen to backend and reading user from response during login

--- views/HomeScreen.tsx
+++ views/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView, TouchableOpacity, Modal, TextInput, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Footer from '../components/Footer';
@@ -13,16 +13,52 @@ const HomeScreen: React.FC<{ route: any }> = ({ route }) => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const reduxUserGroups = useSelector((state: RootState) => state.user.groups);
-  const [groups, setGroups] = useState<Group[]>(reduxUserGroups);
-  const addNewGroup = (): void => {
+  const userEmail = useSelector((state: RootState) => state.user.email);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newGroupName, setNewGroupName] = useState('');
+  const [newGroupDescription, setNewGroupDescription] = useState('');
+
+  const addNewGroup = async (): Promise<void> => {
     const newGroup: Group = {
-      id: groups.length + 1,
-      name: `Group ${groups.length + 1}`,
-      description: `Description for Group ${groups.length + 1}`,
+      id: reduxUserGroups.length + 1,
+      name: newGroupName,
+      description: newGroupDescription,
+      creator: "onechance@2024.com"
     };
-    setGroups([...groups, newGroup]);
-    dispatch(addGroup(newGroup));
+    const createdGroup = await createGroupApiCall(newGroup);
+
+    setModalVisible(false);
+    setNewGroupName('');
+    setNewGroupDescription('');
+    if (createdGroup) {
+      dispatch(addGroup(newGroup));
+    } else {
+      Alert.alert('Error', 'Failed to create a new group.');
+    }
   };
+
+  const createGroupApiCall = async (group: Group): Promise<Group | null> => {
+    try {
+      const response = await fetch('http://10.0.2.2:8080/api/groups', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(group),
+      });
+      console.log(response)
+      if (!response.ok) {
+        throw new Error('Failed to create group');
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error('Error during API call:', error);
+      return null;
+    }
+  };
+
   const GroupCard: React.FC<{ name: string; description: string }> = ({ name, description }) => (
     <TouchableOpacity
       style={styles.card}
@@ -40,14 +76,40 @@ const HomeScreen: React.FC<{ route: any }> = ({ route }) => {
             <Text>Welcome to SplitWise Clone!</Text>
             {/* Additional components like buttons or links */}
             <ScrollView>
-              {groups.map(group => (
+              {reduxUserGroups.map(group => (
                 <GroupCard key={group.id} name={group.name} description={group.description} />
               ))}
             </ScrollView>
-            <Button title="Add New Group" onPress={addNewGroup} />
+            <Button title="Add New Group" onPress={() => setModalVisible(true)} />
           </View>
         </View>
       </ScrollView>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+              placeholder="Group Name"
+              style={styles.input}
+              onChangeText={setNewGroupName}
+              value={newGroupName}
+            />
+            <TextInput
+              placeholder="Group Description"
+              style={[styles.input, styles.descriptionInput]}
+              onChangeText={setNewGroupDescription}
+              value={newGroupDescription}
+            />
+            <View style={styles.buttonRow}>
+              <Button title="Create Group" onPress={addNewGroup} />
+              <Button title="Cancel" onPress={() => setModalVisible(false)} />
+            </View>
+          </View>
+        </View>
+      </Modal>
       <Footer />
     </View>
   );
@@ -74,6 +136,56 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    width: '90%', // Set the width of the modal
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    width: '80%',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  input: {
+    backgroundColor: '#f2f2f2', // Light grey background
+    borderRadius: 10, // Curved corners
+    width: '80%', // Default width
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  descriptionInput: {
+    height: 80, // Make the height larger for description
+  },
+
 });
 
 export default HomeScreen;
